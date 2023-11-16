247f522628e96ed8da9969387fe735c0c1495a86
niffer-dev <jennyngini10@gmal.com>
2023-11-16 12:29:38 +0100

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+const dd = document.querySelector(".inputD")
+const mm = document.querySelector(".inputM")
+const yyyy = document.querySelector(".inputY")
+const submit = document.querySelector(".subbtn")
+const errorAlert = document.querySelector(".modal-bg")
+const errorMessage = document.querySelector(".error-message")
+
+
+submit.addEventListener("click", function(){
+    if(dd.value === "" || mm.value === "" || yyyy.value === ""){
+        validateErrors("Please fill in the field")
+    }
+    else if(!Number(dd.value) || !Number(mm.value) || !Number(yyyy.value)){
+        validateErrors("Please both fields should be numbers")
+    } 
+    else{
+        const calyyy = Math.abs(new Date().getFullYear() - Number(yyyy.value))
+        const calmm = Math.abs(new Date().getMonth() + 1 - Number(mm.value))
+        const caldd = Math.abs(new Date().getDate() - Number(dd.value))
+        document.querySelector('.y').textContent = ""+ calyyy
+        document.querySelector('.m').textContent = ""+ calmm
+        document.querySelector('.d').textContent = ""+ caldd
+    }
+})
+
+function validateErrors(msg){
+    errorAlert.style.display = "flex"
+    errorMessage.textContent = msg
+
+    setTimeout(function(){
+        errorAlert.style.display = "none"
+        errorMessage.textContent = ""
+    },5000)
+}
+
+
+document.querySelector(".btn").addEventListener("click", () => {
+    errorAlert.style.display = "none"
+    errorMessage.textContent = ""
+})
